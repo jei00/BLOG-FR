654789733ea5609bf120fa6d8230fde14defa241
Jei00 <goncad393@gmail.com>
2022-06-20 10:13:39 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./PostList.js";
+import {Router} from "./lib/vaadir-router.js";
+const outlet=document.querySelector('main');
+const router = new Router(outlet);
+
+router.setRoutes([
+    {path:'/',component: 'post-list'},
+]);
+
+console.log('start js ok...');
+document.getElementById(id).style.property = new style
